b8027880316d68f321918b2e1bdd599e22fefa57
Djerad <wafa.djerad@live.fr>
2019-08-18 16:03:31 +0200

    Create main.py

---
+++ Projects/RPG-Battle/main.py
@@ -0,0 +1,10 @@
+from classes.game import Person, Colors
+
+
+magic = [{"name":"Fire", "cost":10, "dmg":50},
+{"name":"Thunder", "cost":10, "dmg":66},
+{"name":"Blizard", "cost":10, "dmg":60}]
+
+
+player = Person(440, 65, 60, 35, magic)
+
