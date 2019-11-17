d61dcda12db11eb6f5c7bc0cc23ef824265d4310
Djerad <wafa.djerad@live.fr>
2019-11-17 14:39:17 +0100

    Create analyser_de_relevés_de_température.py

---
+++ Algorithms/analyser_de_relevés_de_température.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of temperatures to analyse
+for i in input().split():
+    # t: a temperature expressed as an integer ranging from -273 to 5526
+    t = int(i)
+
+print("result")
