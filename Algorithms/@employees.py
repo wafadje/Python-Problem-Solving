01e34672d5c636ab1f21ca8bddcf4e0cce194bf0
Djerad <wafa.djerad@live.fr>
2019-12-24 17:25:34 +0100

    Update @employees.py

--- Algorithms/@employees.py
+++ Algorithms/@employees.py
@@ -1,1 +1,27 @@
-Class employe():
+class Employee:
+  
+  def __init__(self, fname, lname, comp):
+    self.fname = fname
+    self.lname = lname
+    self.comp = comp
+    
+    
+   def email(self):
+    return self.fname + '.' + self.lname + '@' + self.comp + '.com'
+  
+  def fullname(self):
+    return '{} {}'.format(self.fname, self.lname)
+
+
+
+emp_1 = Employee('alan', 'preciado', 50)
+emp_2 = Employee('dania', 'arce', 60)
+
+
+print(emp_1.email)
+print(emp_2.email)
+
+print(emp_1.fullname())
+print(emp_2.fullname())
+    
+    
